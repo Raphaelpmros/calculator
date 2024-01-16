c18e82d0aa413f07bd2b6b5ee70e4c618e9f08b2
Raphael Mello <raphaelpmros@hotmail.com>
2024-01-16 17:50:05 -0300

    basico do html, css e começando o js

---
+++ src/app.js
@@ -0,0 +1,25 @@
+const displayCalculadora = document.querySelector("#display-value");
+const clearButton = document.querySelector("#reset");
+const parenthesesButton = document.querySelector("#parenteses");
+const percentageButton = document.querySelector("#porcentagem");
+const dividedButton = document.querySelector("#divide");
+const sevenButton = document.querySelector("#sete");
+const eightButton = document.querySelector("#oito");
+const nineButton = document.querySelector("#nove");
+const timesButton = document.querySelector("#multiplica");
+const fourButton = document.querySelector("#quatro");
+const fiveButton = document.querySelector("#cinco");
+const sixButton = document.querySelector("#seis");
+const minusButton = document.querySelector("#subtrai");
+const oneButton = document.querySelector("#um");
+const twoButton = document.querySelector("#dois");
+const threeButton = document.querySelector("#tres");
+const plusButton = document.querySelector("#soma");
+const negativeButton = document.querySelector("#negativo");
+const zeroButton = document.querySelector("#zero");
+const decimalButton = document.querySelector("#virgula");
+const resultButton = document.querySelector("#resultado");
+
+let currentValue = '0';
+
+

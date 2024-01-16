3ced754fad533eac70f9fc69821c71cd7cfdcc0c
Raphael Mello <raphaelpmros@hotmail.com>
2024-01-16 18:01:32 -0300

    refazendo o js a partir de classes do html

--- src/app.js
+++ src/app.js
@@ -2,23 +2,11 @@ const displayCalculadora = document.querySelector("#display-value");
 const clearButton = document.querySelector("#reset");
 const parenthesesButton = document.querySelector("#parenteses");
 const percentageButton = document.querySelector("#porcentagem");
-const dividedButton = document.querySelector("#divide");
-const sevenButton = document.querySelector("#sete");
-const eightButton = document.querySelector("#oito");
-const nineButton = document.querySelector("#nove");
-const timesButton = document.querySelector("#multiplica");
-const fourButton = document.querySelector("#quatro");
-const fiveButton = document.querySelector("#cinco");
-const sixButton = document.querySelector("#seis");
-const minusButton = document.querySelector("#subtrai");
-const oneButton = document.querySelector("#um");
-const twoButton = document.querySelector("#dois");
-const threeButton = document.querySelector("#tres");
-const plusButton = document.querySelector("#soma");
 const negativeButton = document.querySelector("#negativo");
-const zeroButton = document.querySelector("#zero");
 const decimalButton = document.querySelector("#virgula");
 const resultButton = document.querySelector("#resultado");
+const number = document.querySelectorAll(".number");
+const operadores = document.querySelectorAll(".operador");
 
 let currentValue = '0';
 
